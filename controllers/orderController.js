405b70672de7d73cc2073eff855996ffc256e154
levanthien1812 <20521947@gm.uit.edu.vn>
2023-05-28 20:50:02 +0700

    fix order

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,55 +1,64 @@
-import catchAsync from '../utils/catchAsync.js'
-import AppError from '../utils/appError.js'
-import Order from '../models/order.js'
-import Cart from '../models/cart.js'
-import ShippingInfo from '../models/shippingInfo.js'
+import catchAsync from "../utils/catchAsync.js";
+import AppError from "../utils/appError.js";
+import Order from "../models/order.js";
+import Cart from "../models/cart.js";
+import ShippingInfo from "../models/shippingInfo.js";
 
 export const createOrder = catchAsync(async (req, res, next) => {
-    // Splits carts string of ids into arrays of ids
-    const ids = req.query.carts.split(',')
+  // Splits carts string of ids into arrays of ids
+  const { cartItems, shippingInfo, paymentMethod, shippingFee, totalPrice } =
+    req.body;
+  const cartItemIds = cartItems.map((cartItem) => cartItem._id);
 
-    const newOrder = await Order.create({
-        user: req.user._id,
-        carts: ids,
-        shippingInfo: req.query.shippingInfo,
-        paymentMethod: req.query.paymentMethod
-    })
+  const newOrder = await Order.create({
+    user: req.user._id,
+    cartItems: cartItemIds,
+    shippingInfo: shippingInfo._id,
+    paymentMethod,
+    shippingFee,
+    totalPrice,
+  });
 
-    // Unactive cart(s) after creating order
-    ids.forEach(async id => {
-        await Cart.findByIdAndUpdate(id, {
-            active: false
-        })
-    })
+  // Unactive cart(s) after creating order
+  cartItemIds.forEach(async (id) => {
+    await Cart.findByIdAndUpdate(id, {
+      active: false,
+    });
+  });
 
-    return res.status(200).json({
-        status: 'success',
-        data: newOrder
-    })
-})
+  return res.status(200).json({
+    status: "success",
+    data: newOrder,
+  });
+});
 
 export const getAllOrders = catchAsync(async (req, res, next) => {
-    const orders = await Order.find({ user: req.user._id })
-    
-    return res.status(200).json({
-        status: 'success',
-        quantity: orders.length,
-        data: orders
-    })
-})
+  const status = req.query.status || null;
+  let orders = [];
+  console.log(status)
+  if (!status) {
+    orders = await Order.find({ user: req.user._id });
+  } else orders = await Order.find({ user: req.user._id, status });
+
+  return res.status(200).json({
+    status: "success",
+    quantity: orders.length,
+    data: orders,
+  });
+});
 
 export const deleteOrder = catchAsync(async (req, res, next) => {
-    const order = await Order.findById(req.params.id)
-    // Không được hủy đơn hàng sau khi giao cho đơn vị vận chuyển
-    if (order.state.includes('giao cho đơn vị vận chuyển')) {
-        return res.status(200).json({
-            status: 'fail',
-            message: 'Can not cancel order! Your parcel is being shipped to you.'
-        })
-    }
+  const order = await Order.findById(req.params.id);
+  // Không được hủy đơn hàng sau khi giao cho đơn vị vận chuyển
+  if (order.state.includes("giao cho đơn vị vận chuyển")) {
+    return res.status(200).json({
+      status: "fail",
+      message: "Can not cancel order! Your parcel is being shipped to you.",
+    });
+  }
 
-    await Order.findOneAndDelete(req.params.id)
-    res.status(200).json({
-        status: 'success'
-    })
-})
+  await Order.findOneAndDelete(req.params.id);
+  res.status(200).json({
+    status: "success",
+  });
+});
--- models/order.js
+++ models/order.js
@@ -7,17 +7,18 @@ const OrderSchema = new mongoose.Schema({
         require: true,
         ref: 'User'
     },
-    carts: [{
+    cartItems: [{
         type: mongoose.Schema.Types.ObjectId,
         require: true,
         ref: 'Cart'
     }],
-    shipPrice: {
+    shippingFee: {
         type: Number,
         default: 0
     },
     totalPrice: {
-        type: Number
+        type: Number,
+        default: 0
     },
     shippingInfo: {
         type: mongoose.Schema.Types.ObjectId,
@@ -25,29 +26,30 @@ const OrderSchema = new mongoose.Schema({
     },
     status: {
         type: String,
+        default: "pending",
         enum: {
-            values: ['Đơn hàng đã được đặt', 'Người gửi đang chuẩn bị hàng', 'Đang vận chuẩn', 'Giao hàng không thành công', 'Đã giao']
+            values: ['pending', 'shipping', 'received', 'cancelled']
         }
     },
     paymentMethod: {
         type: String,
         require: [true, 'Please choose your payment method!'],
         enum: {
-            values: ['Chuyển khoản', 'Thanh toán khi nhận hàng']
+            values: ['transfer', 'cash']
         }
     }
 })
 
-OrderSchema.pre('save', async function (next) {
-    // Calculate total price
-    let cartsPrice = 0
-    await Promise.all(this.carts.map(async item => {
-        const cart = await Cart.findById(item)
-        cartsPrice += cart.price
-    }))
+// OrderSchema.pre('save', async function (next) {
+//     // Calculate total price
+//     let cartsPrice = 0
+//     await Promise.all(this.cartItems.map(async item => {
+//         const cart = await Cart.findById(item)
+//         cartsPrice += cart.price
+//     }))
     
-    this.totalPrice = this.shipPrice + cartsPrice
-    next()
-})
+//     this.totalPrice = this.shipPrice + cartsPrice
+//     next()
+// })
 
 export default mongoose.model('Order', OrderSchema)
